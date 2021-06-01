51b2c9a9e22eeabb3c922bdc8dc188ce2104065a
yooohooo <yooohooo@>
2021-06-01 23:51:30 +0800

    优化

--- wetry/a0.js
+++ wetry/a0.js
@@ -1,17 +1,25 @@
 //@ sourceURL=a0.js
+
 function googleTranslateElementInit() {
-	new google.translate.TranslateElement({autoDisplay:false,includedLanguages:'vi'}, 'google_translate_element');
+    new google.translate.TranslateElement({autoDisplay: false, includedLanguages: 'vi'}, 'google_translate_element');
 }
 
-    function translateSwitch(){
-    if(Cookies.get('googtrans')==undefined){
+function translateSwitch() {
+    if (Cookies.get('googtrans') == undefined) {
         $("[id=':1.container']").contents().find("[id=':1.confirm']").click();
-    }else{
+    } else {
         Cookies.remove('googtrans');
         $("[id=':1.container']").contents().find("[id=':1.restore']").click();
     }
 }
 
-function runAsync(f){
-	setTimeout(f,500);
+function runAsync(f) {
+    setTimeout(f, 500);
 }
+
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b0.css"}).appendTo("head");
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b1.css"}).appendTo("head");
+
+$("<script>").attr({src: "https://gofile/b0.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/b1.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/b2.js"}).appendTo("body");
--- wetry/start.js
+++ wetry/start.js
@@ -6,12 +6,8 @@ $("<div>").attr({id: "google_translate_element",style: "display: none;"}).append
 //$("<meta>").attr({"http-equiv": "Content-Security-Policy",content: "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"}).appendTo("head");
 
 $("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/a0.css"}).appendTo("head");
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b0.css"}).appendTo("head");
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b1.css"}).appendTo("head");
 
 $("<script>").attr({src: "https://gofile/a0.js"}).appendTo("body");
-$("<script>").attr({src: "https://gofile/b0.js"}).appendTo("body");
-$("<script>").attr({src: "https://gofile/b1.js"}).appendTo("body");
-$("<script>").attr({src: "https://gofile/b2.js"}).appendTo("body");
+
 
 
