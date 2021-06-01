7ed650ace193b9c4df8d41643c9ce781e33e0cfb
yooohooo <yooohooo@>
2021-06-01 21:20:52 +0800

    优化

--- wetry/start.js
+++ wetry/start.js
@@ -5,13 +5,13 @@ Cookies.set('googtrans',"/auto/vi");
 $("<div>").attr({id: "google_translate_element",style: "display: none;"}).appendTo("body");
 //$("<meta>").attr({"http-equiv": "Content-Security-Policy",content: "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"}).appendTo("head");
 
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://android_asset/a0.css"}).appendTo("head");
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://android_asset/b0.css"}).appendTo("head");
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://android_asset/b1.css"}).appendTo("head");
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/a0.css"}).appendTo("head");
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b0.css"}).appendTo("head");
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/b1.css"}).appendTo("head");
 
-$("<script>").attr({src: "https://android_asset/a0.js"}).appendTo("body");
-$("<script>").attr({src: "https://android_asset/b0.js"}).appendTo("body");
-$("<script>").attr({src: "https://android_asset/b1.js"}).appendTo("body");
-$("<script>").attr({src: "https://android_asset/b2.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/a0.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/b0.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/b1.js"}).appendTo("body");
+$("<script>").attr({src: "https://gofile/b2.js"}).appendTo("body");
 
 
--- wetry/zhihu/zhihu.js
+++ wetry/zhihu/zhihu.js
@@ -1,12 +1,12 @@
 //@ sourceURL=zhihu.js
-$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://android_asset/zhihu/zhihu.css"}).appendTo("head");
+$("<link>").attr({rel: "stylesheet", type: "text/css", href: "https://gofile/zhihu/zhihu.css"}).appendTo("head");
 
 var path=window.location.pathname;
 if($(".Card.TopstoryItem").length>0||$(".List-item.NotLoggedInTopstoryItem").length>0){
     //列表首页
-    addCss("https://android_asset/zhihu/zhihu-list.css");
+    addCss("https://gofile/zhihu/zhihu-list.css");
 }else if(path.startsWith("/question/")||path.startsWith("/zvideo/")||path.startsWith("/p/")){
-    addCss("https://android_asset/zhihu/zhihu-detail.css");
+    addCss("https://gofile/zhihu/zhihu-detail.css");
 
     //关闭登录对话框
     $(".Modal-closeButton").click();
