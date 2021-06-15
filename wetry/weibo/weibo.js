f630328163ccbcdaab4a0c826e6d23e96926e7e3
yooohooo <yooohooo@>
2021-06-16 00:35:55 +0800

    優化weibo.js

--- wetry/weibo/weibo.js
+++ wetry/weibo/weibo.js
@@ -1,11 +1,10 @@
 //@ sourceURL=weibo.js
 
+addCss("https://gofile/weibo/weibo.css");
+
 let href = window.location.href;
 if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) {
-    window.location.href = "https://weibo.com/login.php";
-} else if (href.startsWith("https://weibo.com/login.php")) {
-    $("[node-type='loginBtn']").click();
-    addCss("https://gofile/weibo/weibo-login.css");
+    window.location.href = "https://passport.weibo.cn/signin/login";
 }
 
 function addCss(cssUrl) {
