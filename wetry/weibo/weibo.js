a8744918d6e0a1c6dbdd031db232aa9d54663d20
yooohooo <yooohooo@>
2021-06-15 23:51:14 +0800

    add weibo-login.css

--- wetry/weibo/weibo.js
+++ wetry/weibo/weibo.js
@@ -4,6 +4,7 @@ let href = window.location.href;
 if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) {
     window.location.href = "https://weibo.com/login.php";
 } else if (href.startsWith("https://weibo.com/login.php")) {
+    $("[node-type='loginBtn']").click();
     addCss("https://gofile/weibo/weibo-login.css");
 }
 
