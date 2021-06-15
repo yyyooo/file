3a8eff79531043d07418ad0dca541879017db187
yooohooo <yooohooo@>
2021-06-16 01:32:51 +0800

    优化weibo.js

--- wetry/weibo/weibo.js
+++ wetry/weibo/weibo.js
@@ -7,10 +7,6 @@ if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) 
     window.location.href = "https://passport.weibo.cn/signin/login";
 }
 
-$(".verify-box").click(()=>{
-    window.location.href = "https://passport.weibo.cn/signin/login";
-})
-
 function addCss(cssUrl) {
     $("<link>").attr({rel: "stylesheet", type: "text/css", href: cssUrl}).appendTo("head");
 }
