0c62260160b452d0997c770ed5663677da9938c9
yooohooo <yooohooo@>
2021-08-01 01:03:49 +0800

    优化weibo.js

--- wetry/weibo/weibo.js
+++ wetry/weibo/weibo.js
@@ -1,23 +1,40 @@
 //@ sourceURL=weibo.js
 
-addCss("https://gofile/weibo/weibo.css");
+myPostInit();
 
-let href = window.location.href;
-if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) {
-    //手机号无需翻译
-    $('.code-text').addClass('notranslate');
+function myPostInit() {
+    addCss("https://gofile/weibo/weibo.css");
 
-    let expandSelect = () => {
-        let event = document.createEvent('MouseEvents');
-        event.initMouseEvent('mousedown', true, true, window);
-        $(".box-select")[0].dispatchEvent(event)
+    if (window.location.pathname.startsWith("/iforgot/choose")) {
+        //手机号无需翻译
+        $('.checkPhone').addClass('notranslate');
+        return;
     }
-    $('.code-text').click(expandSelect);
-    $('.select-icon').click(expandSelect);
-} else if (window.location.pathname.startsWith("/iforgot/choose")) {
-    //手机号无需翻译
-    $('.checkPhone').addClass('notranslate');
+
+    let href = window.location.href;
+    if (href === "https://m.weibo.cn/") {
+        setTimeout(() => dealLoginPage(), 1500);
+    }
+
+    dealLoginPage();
 }
+
+function dealLoginPage() {
+    if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) {
+        //手机号无需翻译
+        $('.code-text').addClass('notranslate');
+
+        let expandSelect = () => {
+            let event = document.createEvent('MouseEvents');
+            event.initMouseEvent('mousedown', true, true, window);
+            $(".box-select")[0].dispatchEvent(event)
+        }
+        $('.code-text').click(expandSelect);
+        $('.select-icon').click(expandSelect);
+    }
+}
+
+
 
 
 
