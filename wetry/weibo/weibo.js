82de3d65e4d1fe99712de6456bac27e08dc4a353
yooohooo <yooohooo@>
2021-08-01 00:46:01 +0800

    优化weibo.js

--- wetry/weibo/weibo.js
+++ wetry/weibo/weibo.js
@@ -2,16 +2,23 @@
 
 addCss("https://gofile/weibo/weibo.css");
 
-    let href = window.location.href;
+let href = window.location.href;
 if (href.startsWith("https://m.weibo.cn/login") || $(".verify-box").length > 0) {
-    window.location.replace("https://passport.weibo.cn/signin/login");
-}else{
-    if (window.location.pathname.startsWith("/iforgot/choose")){
-        //手机号无需翻译
-        $('.checkPhone').addClass('notranslate');
-    }
+    //手机号无需翻译
+    $('.code-text').addClass('notranslate');
 
+    let expandSelect = () => {
+        let event = document.createEvent('MouseEvents');
+        event.initMouseEvent('mousedown', true, true, window);
+        $(".box-select")[0].dispatchEvent(event)
+    }
+    $('.code-text').click(expandSelect);
+    $('.select-icon').click(expandSelect);
+} else if (window.location.pathname.startsWith("/iforgot/choose")) {
+    //手机号无需翻译
+    $('.checkPhone').addClass('notranslate');
 }
+
 
 
 
