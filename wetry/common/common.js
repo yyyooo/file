5406676412c5f222fac3853cd9cfd6e526785b4c
yooohooo <yooohooo@>
2021-08-13 23:39:48 +0800

    优化weibo.js

--- wetry/common/common.js
+++ wetry/common/common.js
@@ -1,9 +1,16 @@
 //@ sourceURL=common.js
 
-let redirectUrl = Cookies.get('mapp_redirect');
-if (!isEmpty(redirectUrl)) {
-    Cookies.remove("mapp_redirect");
-    window.location.replace(redirectUrl);
+doInit();
+
+function doInit() {
+    let redirectUrl = Cookies.get('mapp_redirect');
+    if (!isEmpty(redirectUrl)) {
+        Cookies.remove("mapp_redirect");
+        window.location.replace(redirectUrl);
+        return;
+    }
+
+    unregisterSw();
 }
 
 function isEmpty(obj) {
@@ -12,4 +19,19 @@ function isEmpty(obj) {
 
 function addCss(cssUrl) {
     $("<link>").attr({rel: "stylesheet", type: "text/css", href: cssUrl}).appendTo("head");
+}
+
+function unregisterSw() {
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+
+    let serviceWorker = navigator.serviceWorker;
+    serviceWorker.getRegistrations ? serviceWorker.getRegistrations().then(function (sws) {
+        sws.forEach(function (sw) {
+            sw.unregister();
+        });
+    }) : serviceWorker.getRegistration && serviceWorker.getRegistration().then(function (sw) {
+        sw && sw.unregister();
+    });
 }
